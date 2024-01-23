00e878dfee3efc088e4e97d749a88d3a59e2b3d8
Alan Almeida <alandavidalmeida@gmail.com>
2024-01-23 18:45:25 -0300

    fixing

--- PerisferiaStore-dev/frontend/src/context/DataContext.jsx
+++ PerisferiaStore-dev/frontend/src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import { useReducer } from 'react'
 import { useEffect } from 'react';
 import axios from 'axios'
 import { createContext, useState } from "react";
