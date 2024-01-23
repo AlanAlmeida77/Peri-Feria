a0320e08a39d7d4adf1f169117900de7363fac12
Alan Almeida <alandavidalmeida@gmail.com>
2024-01-23 18:49:02 -0300

    changes

--- PerisferiaStore-dev/frontend/src/context/DataContext.jsx
+++ PerisferiaStore-dev/frontend/src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'
+import React , { useReducer } from 'react'
 import { useEffect } from 'react';
 import axios from 'axios'
 import { createContext, useState } from "react";
